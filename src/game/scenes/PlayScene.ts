b45a268e1e26c5b9baca9e27f7c23005ca3e877d
Daniel Ahn <et3rnal.d@gmail.com>
2023-09-18 22:55:01 -0700

    save point

--- src/game/scenes/PlayScene.ts
+++ src/game/scenes/PlayScene.ts
@@ -1,25 +1,33 @@
 import { Scene } from 'phaser'
 
 export default class PlayScene extends Scene {
-  constructor () {
+  constructor() {
     super({ key: 'PlayScene' })
   }
 
-  create () {
+  create() {
     this.add.image(400, 300, 'sky')
 
-    const bomb = this.physics.add.image(400, 200, 'bomb')
-    bomb.setCollideWorldBounds(true)
-    bomb.body.onWorldBounds = true // enable worldbounds collision event
-    bomb.setBounce(1)
-    bomb.setVelocity(200, 20)
+    const bombs = []
+    for (let i=0; i < 21; i++ ) {
+      const bomb = this.physics.add.image(100 + i * 50, 100, 'bomb')
+      bomb.setCollideWorldBounds(true)
+      bomb.setBounce(1)
+      bomb.setVelocity(150, 0)
+      bombs.push(bomb)
+    }
+
+    for (let i = 0; i < bombs.length; i++) {
+      for (let j = i + 1; j < bombs.length; j++) {
+        this.physics.add.collider(bombs[i], bombs[j])
+      }
+    }
 
     this.sound.add('thud')
     this.physics.world.on('worldbounds', () => {
-      this.sound.play('thud', { volume: 0.75 })
+      this.sound.play('thud', { volume: 0.01 })
     })
   }
 
-  update () {
-  }
+  update() {}
 }
